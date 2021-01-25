48d604c60c0efcda8d1ea6a1cab49ace5187c609
JRGCast <joao.ralph1@gmail.com>
2021-01-25 15:14:00 -0300

    requisito 1 parcialmente feito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import './App.css';
+import { EditMovie, MovieDetails, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <h1 style={ { textAlign: 'center', color: 'grey' } }>Movie Card Library CRUD</h1>
+      <nav className="navBar">
+        <Link to="/">Home</Link>
+        <Link to="/movieDetails">Movie Details</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
