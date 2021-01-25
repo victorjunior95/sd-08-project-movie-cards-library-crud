fde8e437618d7ba273ea4082d8b0e830df3bbc54
Seu nome <luiza.dilly@gmail.com>
2021-01-25 15:50:56 -0300

    commit inicial

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="404-error">
+        <p>Page did not found</p>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -8,15 +8,25 @@ class MovieList extends Component {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
+  }
+
+  componentDidMount() {
+    // movieAPI.getMovie().then((data) => {
+    //   this.setState({
+    //     loading: !loading,
+    //     movies: data,
+    //   });
+    // });
   }
 
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
-
+    // if (loading) return <h1>Carregando...</h1>;
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
