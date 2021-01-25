fccf85bcdb79c3eab886c447539ba8ba3685b13d
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-01-25 17:09:35 -0300

    REQ 1: Added Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
@@ -11,11 +11,13 @@ function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
-      <Route exact path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
-      <Route path="/movies/new" component={ NewMovie } />
-      <Route path="/movies/:id/edit" component={ EditMovie } />
-      <Route default component={ NotFound } />
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
