ac23ff574ab5a6fec909b5c867f616781d0d26ca
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-25 15:03:12 -0300

    Correçao lint Req1

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
     <Router>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route exact path="/" component={MovieList } />
-        <Route path="/movies/:id" component={MovieDetails } />
-        <Route exact path="/movies/new" component={NewMovie } />
-        <Route path="/movies/:id/edit" component={EditMovie } />
-        <Route component={NotFound } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
       </Switch>
     </Router>
   );
