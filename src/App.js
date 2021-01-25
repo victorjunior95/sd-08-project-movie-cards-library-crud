affe5ec7d9703d3afd4d454b486d0293f8f1b131
Alana Monteiro <lanaah.15@gmail.com>
2021-01-25 17:05:08 -0300

    Commit Inicial - Alana Monteiro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import EditMovie from './pages/EditMovie'
+import MovieList from './pages/MovieList'
+import NewMovie from './pages/NewMovie'
+import NotFound from './pages/NotFound'
+
+
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" render={(props) => props.id } />
+          <Route path="/new" component={ NewMovie } />
+          <Route path="FALTA PREENCHER" component={ NotFound }/>
+          <Route path="/" component=  { MovieList } />
+        </Switch>
+      </BrowserRouter>
+      <h1>Movie Card Library CRUD</h1>
+    </div>
   );
 }
 
