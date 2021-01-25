96ff2be18382ec1645e2ea5e4746ef81d09d9317
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-25 18:54:28 -0300

    Ultima rota corrigida

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { MovieForm } from '../components';
+// import { MovieForm } from '../components';
 // import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
