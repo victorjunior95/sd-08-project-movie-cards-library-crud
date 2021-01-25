ac360bb0b2195ab6d1826c254ea3df043e8103a2
Lucas Gama <lucasbg@protonmail.com>
2021-01-25 15:09:04 -0300

    Requisito 1: Browser router no component app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/" component={ MovieList } />
+        <Route component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
