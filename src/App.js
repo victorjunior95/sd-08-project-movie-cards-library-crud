84b175e9fd4b04c17a529280dea7d62431a88fd9
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-01-25 17:47:10 -0300

    Correçao requisito 1

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ function App() {
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route exact path="/" component={ MovieList } />
-        <Route
-          path="/movies/:id"
-          render={ (props) => <MovieDetails { ... props } id="id" /> }
-        />
         <Route path="/movies/new" component={ NewMovie } />
         <Route
           path="/movies/:id/edit"
           render={ (props) => <EditMovie { ...props } id="id" /> }
         />
-        <Route path="" component={ NotFound } />
+        <Route
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } id="id" /> }
+        />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/" component={ NotFound } />
       </Switch>
     </BrowserRouter>
   );
