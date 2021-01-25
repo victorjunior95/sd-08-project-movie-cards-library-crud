7cc8ea5c24de00ae6fd1ef6c2b7629da5eb73eb6
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-25 17:30:41 -0300

    Requisito 3- Renderizando os MOvieCards com as informaçoes. E o projeto começa a ganhar forma

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const {
+      movie: { id, title, storyline, subtitle, rating, imagePath },
+    } = this.props;
+    // esse trecho ( linhas 6,7,8 ) foi retirado da pagina https://github.com/tryber/sd-07-project-movie-card-library-crud/pull/144/files
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt={ title } />
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <h3>{storyline}</h3>
+        <h4>{rating}</h4>
+        <p>
+          <Link to={ `movies/${id}` }>VER DETALHES</Link>
+        </p>
       </div>
     );
   }
 }
-
 export default MovieCard;
