586e260b1415d05bc9b09f5e744b22fdae36178b
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-25 14:41:57 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { MovieList,
+  MovieDetails,
+  NewMovie,
+  EditMovie,
+  NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route path="/" render={ () => <MovieList /> } />
+      <Route path="/movies/:id" render={ () => <MovieDetails /> } />
+      <Route path="/movies/new" render={ () => <NewMovie /> } />
+      <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+      <Route path="" component={ NotFound } />
+
+    </BrowserRouter>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
-import * as movieAPI from '../services/movieAPI';
+// import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +10,36 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: false,
     };
   }
 
+  componentDidMount() {
+    console.log('oi');
+  }
+
+  addStateMovies() {
+    this.setState({
+      loading: true,
+    },
+    async () => {});
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      <div>
+        {
+          loading ? <Loading />
+            : (
+              <div data-testid="movie-list">
+                {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+              </div>
+            )
+        }
       </div>
     );
   }
