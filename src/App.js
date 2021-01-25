ab9abaa262a07fff8a848c4cd53d293e6a935208
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-25 15:02:16 -0300

    Requisito 1: Estruturando Browser Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={MovieList } />
+        <Route path="/movies/:id" component={MovieDetails } />
+        <Route exact path="/movies/new" component={NewMovie } />
+        <Route path="/movies/:id/edit" component={EditMovie } />
+        <Route component={NotFound } />
+      </Switch>
     </Router>
   );
 }
