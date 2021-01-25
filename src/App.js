c38c0dfea9cd8973d63ce69a9735d1289b87aa34
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-01-25 14:56:06 -0300

    CRUD Movie Card Library Hugo Leonardo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import { MovieDetails, NewMovie, EditMovie } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id:edit" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        {/* <Route exact path="/" render={ () => new MovieList() } /> */}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
