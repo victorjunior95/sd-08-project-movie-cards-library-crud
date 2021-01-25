a05073357999640099a095f9b834638c1cb2803f
Paulo Simões <paulohbsimoes@gmail.com>
2021-01-25 15:17:32 -0300

    Requisito 2 concluído

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,16 +9,26 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then((movies) => {
+      this.setState({
+        movies,
+        loading: false,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
+        { loading && <p>Carregando...</p> }
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
