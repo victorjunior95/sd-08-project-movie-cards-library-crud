6bc9c70c639a86e5bae5ebdf477ae890707888b5
Leandro Lachini <leandrolachini@gmail.com>
2021-01-25 17:31:50 -0300

    task 1 resolved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD!</div>
+    <BrowserRouter>
+      <Route exact path="/" component={ MovieList } />
+      <Route exact path="/movies/:id">
+        <MovieDetails />
+      </Route>
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </BrowserRouter>
   );
 }
 
