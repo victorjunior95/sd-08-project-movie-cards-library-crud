d73e8e24dc3a93d53473997fb78cbc6c87390818
Victor Miwa <victoryukimiwa@hotmail.com>
2021-01-25 18:31:38 -0300

    requisito 3 MovieCard com tag Link finalizado

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, imagePath, storyline, id } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <div>
+          <img src={ imagePath } alt="capa-filme" />
+        </div>
+        <div>
+          <p>{title}</p>
+        </div>
+        <div>
+          <p>{storyline}</p>
+        </div>
+        <div>
+          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
