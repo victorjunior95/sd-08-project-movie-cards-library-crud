7c98a0bcbb5d9fd56ae702144a52b17bc5b57bfe
albertassi88 <albertassi88@gmail.com>
2021-01-25 15:20:07 -0300

    Criacao das rotas no componente App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+import React from 'react';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          Movie Card Library CRUD
+          <MovieList />
+          <MovieDetails />
+          <NewMovie />
+          <EditMovie />
+          <NotFound />
+        </div>
+      </BrowserRouter>      
+    );
+  }
 }
 
 export default App;
-
-
