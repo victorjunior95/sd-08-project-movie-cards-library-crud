6af3185ea5671af774422fc48b698955ed49a166
Lucas Gama <lucasbg@protonmail.com>
2021-01-25 15:48:37 -0300

    Requisito 3: link de detalhes para cada moviecard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id, imagePath } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{title}</h1>
+        <img src={ imagePath } alt={ title } />
+        <p>{storyline}</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
