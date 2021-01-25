abb9575a968e18a12ff60ef1ba33321f3f6305e6
Victor Miwa <victoryukimiwa@hotmail.com>
2021-01-25 18:32:14 -0300

    requisito 4 renderizando MovieDetails finalizado

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import * as movieAPI from '../services/movieAPI';
+import { getMovie } from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: [],
+    };
 
+    this.findMovie = this.findMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.findMovie();
+  }
+
+  async findMovie() {
+    const { match: { params: { id } } } = this.props;
+    const result = await getMovie(id);
+    this.setState({
+      movie: result,
+    });
+  }
+
+  contentMovieDetails() {
+    const { movie: { title, storyline, imagePath, genre,
+      rating, subtitle } } = this.state;
     return (
-      <div data-testid="movie-details">
+      <div>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}`}</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
@@ -20,6 +41,23 @@ class MovieDetails extends Component {
       </div>
     );
   }
+
+  render() {
+    const { movie } = this.state;
+    return (
+      <div data-testid="movie-details">
+        { movie.length === 0 ? <Loading /> : this.contentMovieDetails() }
+      </div>
+    );
+  }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
