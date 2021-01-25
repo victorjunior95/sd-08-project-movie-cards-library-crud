cbc6986cb64aed769f4baad2a09cad8d2d13c354
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-25 16:16:19 -0300

    Correçoes em MovieDetails para verificar atualizaçao percentual no gitHub

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
-import * as movieAPI from '../services/movieAPI';
+// import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
+    const { loading } = this.state;
     const { title, storyline, imagePath, genre, rating, subtitle } = {};
 
+    if (loading) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
