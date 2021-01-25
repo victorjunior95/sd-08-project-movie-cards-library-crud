a30094a60b4394071a7a184322b8f1176cb88a27
Lucas Gama <lucasbg@protonmail.com>
2021-01-25 15:38:08 -0300

    MovieList mostra componente loading e faz requisicao de filmes

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +10,34 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies() {
+    this.setState({ loading: true }, async () => {
+      const movies = await movieAPI.getMovies();
+      this.setState({
+        movies,
+        loading: false,
+      });
+    });
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    const { loading, movies } = this.state;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading /> : movies.map(
+          (movie) => <MovieCard key={ movie.title } movie={ movie } />,
+        )}
       </div>
     );
   }
