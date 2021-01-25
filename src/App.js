74716e0d64bb9a6d8c74eddcd7612aa9c5d07389
liisaambrosen <liisa.ambrosen@gmail.com>
2021-01-25 17:32:37 -0300

    Projeto Movie Card Library CRUD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
+import MovieList from './pages/MovieList';
+
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BroswerRouter>
+      <Switch>
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/:id" component={NotFound} />
+        <Route path="/" component={MovieList} />
+      </Switch>
+    </BroswerRouter>
   );
 }
 
