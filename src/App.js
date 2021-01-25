8868c0fc97a058869fe4c88fe72f9eb18ec305ca
TandyFW <tandyfw@gmail.com>
2021-01-25 17:03:01 -0300

    Requisito 1 concluido_1

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
           <Route path="/movies/:id/edit" component={ EditMovie } />
           <Route path="/movies/new" component={ NewMovie } />
           <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="/:id" component={ NotFound } />
           <Route path="/" component={ MovieList } />
-          <Route component={ NotFound } />
         </Switch>
       </BrowserRouter>
     </div>
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -4,14 +4,14 @@ import { MovieForm } from '../components';
 // import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  // handleSubmit(updatedMovie) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  //   this.handleSubmit = this.handleSubmit.bind(this);
   // }
+
+  // // handleSubmit(updatedMovie) {
+  // // }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
