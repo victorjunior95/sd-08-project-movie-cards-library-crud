0364dacbd0b5272dab5f678897bb59b7de413628
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-25 18:14:21 -0300

    Correçao da ordem dos paths -req1

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
     <Router>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route component={ NotFound } />
+        <Route path="/:id" component={ NotFound } />
+        <Route path="/" component={ MovieList } />
       </Switch>
     </Router>
   );
