64049781a676cc65b4d147caa59269c2b406f777
Edson Araujo <araujo.barros@matematica.ufrj.br>
2021-01-25 14:41:22 -0300

    1-BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import NewMovie from './pages/NewMovie';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+    </BrowserRouter>
   );
 }
 
