5a97c27b53d2930feb966819ace341d4c16730de
vivianeflowt <vivianeflowt@gmail.com>
2021-01-25 20:00:47 -0300

    ok

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
+import { Loading, MovieForm } from '../components';
 
-import { MovieForm } from '../components';
-// eslint-disable-next-line
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: undefined,
+      status: false,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
   }
 
   handleSubmit(updatedMovie) {}
 
+  async fetchMovie() {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    const data = await movieAPI.getMovie(id);
+
+    this.setState({ movie: data });
+
+    if (!data) {
+      // return this.redirectNotFound();
+      this.setState({ shouldRedirect: true });
+    }
+    this.setState({ status: true });
+  }
+
+  redirectNotFound() {
+    const {
+      history: { push },
+    } = this.props;
+    push('/notfound');
+  }
+
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return this.redirectNotFound();
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
-    const { id } = this.props.match.params;
+    const { id } = this.state;
 
     return (
       <div data-testid="edit-movie">
@@ -32,5 +60,16 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default EditMovie;
