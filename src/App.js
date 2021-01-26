da1e56af24602d1542750dee6661a85e1fa74f02
Mario Pardo <mariio.pardojr@gmail.com>
2021-01-26 16:12:01 -0400

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="" component={ NotFound } />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
