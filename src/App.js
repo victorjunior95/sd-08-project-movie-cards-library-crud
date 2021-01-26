c8b3f40ca9856d8e8ca3b25b482403ff91c850a1
Silvio Dayube <scarige@gmail.com>
2021-01-26 15:47:10 -0300

    Req 1 - Adds BrowserRouter and Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route path="/" render={ () => <MovieList /> } />
+      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+      <Route path="/movies/new" render={ () => <NewMovie /> } />
+      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      <Route path="*" exact render={ () => <NotFound /> } />
+    </BrowserRouter>
   );
 }
 
