d4d9a99ded5343f18f20f9062ef4a48c2762e7f7
amandaos <amanda.775.souza@gmail.com>
2021-01-25 23:00:39 -0300

    feature: Adicionando direcionamento por routa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { NewMovie, EditMovie, MovieList, MovieDetails, NotFound } from './pages/index';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+        <Route component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
