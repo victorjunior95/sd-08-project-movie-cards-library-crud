21671a222ed6c65e460a22e39a8d4fe3e832608e
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-01-26 13:28:28 -0300

    Complete requirement 5

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
 
+import './App.css';
+
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route component={ NotFound } />
       </Switch>
     </Router>
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 
+import { Loading } from '../components';
+import { Redirect } from 'react-router-dom';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: [],
+      shouldRedirect: false,
+      status: 'loading',
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.fetchMovie(movieAPI.getMovie(id));
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
+  }
+
+  fetchMovie(result) {
+    this.setState(
+      { status: 'loading' },
+      async () => {
+        this.setState({
+          movie: await result,
+          status: 'loaded',
+        });
+      },
+    );
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return (
+        <Loading />
+      );
     }
 
     return (
