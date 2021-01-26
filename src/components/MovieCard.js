7633b0cc50a97ea909f784ff72f0543c1acbb58e
victor718 <victorfelipeoliveira1@gmail.com>
2021-01-26 00:44:23 -0300

    movieList e css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../css/MovieCard.css';
 
 class MovieCard extends React.Component {
-  render() {
+  movieCarImg(imagePath, title) {
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div className="movie-car-img">
+        <img className="movie-img" src={ imagePath } alt={ title } />
+      </div>
+    );
+  }
+
+  movieCarInfo(title, subtitle, genre) {
+    const firstLetter = genre[0].toUpperCase();
+    const genreUpper = genre.replace(genre[0], firstLetter);
+    return (
+      <div className="movie-car-info">
+        <span className="movie-car-title">{title}</span>
+        <span className="movie-car-subtitle">{subtitle}</span>
+        <span className="movie-car-genre">{genreUpper}</span>
+      </div>
+    );
+  }
+
+  movieCarStoryline(storyline) {
+    return (
+      <div className="movie-car-storyline">
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+
+  movieCarRatingBookmarked(rating, bookmarked) {
+    return (
+      <div className="movie-car-rating-bookmarked">
+        <span>{rating}</span>
+        <span>{bookmarked ? 'Favorito' : ''}</span>
+      </div>
+    );
+  }
+
+  render() {
+    const {
+      movie: { title, subtitle, storyline, rating, imagePath, bookmarked, genre },
+    } = this.props;
+    return (
+      <div className="movie-card-content" data-testid="movie-card">
+        {this.movieCarImg(imagePath, title)}
+        {this.movieCarInfo(title, subtitle, genre)}
+        {this.movieCarStoryline(storyline)}
+        {this.movieCarRatingBookmarked(rating, bookmarked)}
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    bookmarked: PropTypes.bool.isRequired,
+    genre: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
+import '../css/MovieList.css';
+
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
+      isLoading: false,
       movies: [],
     };
   }
 
+  componentDidMount() {
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({ isLoading: true },
+      async () => {
+        this.setState({ movies: await movieAPI.getMovies(), isLoading: false });
+      });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, isLoading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    if (isLoading) return <Loading />;
     return (
-      <div data-testid="movie-list">
+      <div className="movie-list-container" data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
