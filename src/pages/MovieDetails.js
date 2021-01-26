d21d0759a6f3abf8bf323604154ca65a20ae81f0
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-26 15:47:34 -0300

    100%

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -44,8 +44,8 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to="/">VOLTAR</Link>
-        <Link to={ `/movies/${id}/edit` } onClick={console.log('ai')}>EDITAR</Link>
-        <Link to="/" onClick={console.log('OI')}>DELETAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/" onClick={ () => movieAPI.deleteMovie(id) }>DELETAR</Link>
       </div>
     );
   }
