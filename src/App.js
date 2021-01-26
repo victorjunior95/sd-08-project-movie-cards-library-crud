6ebcdd357b998e6f2988555dbfc0f834a2183e3b
Victor Breno <victor.brns2@gmail.com>
2021-01-26 13:38:27 -0300

    tentativa do requisito 2

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/" exact component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/" exact component={ MovieList } />
         <Route path="*" component={ NotFound } />
       </Switch>
     </BrowserRouter>
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -7,16 +8,30 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.attMovies = this.attMovies.bind(this);
+
     this.state = {
       movies: [],
+      status: true,
     };
   }
 
-  render() {
+  componentDidMount() {
+    this.attMovies();
+  }
+
+  async attMovies() {
     const { movies } = this.state;
+    const requestMovie = await movieAPI.getMovies();
+    this.setState({ movies: requestMovie }, () => console.log(movies, requestMovie));
+  }
 
-    // Render Loading here if the request is still happening
+  render() {
+    const { movies, status } = this.state;
 
+    if (status) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
