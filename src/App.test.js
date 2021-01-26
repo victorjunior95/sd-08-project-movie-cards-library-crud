76726077e3710d5036b4fef5997c0a91a3a9cf4c
albertassi88 <albertassi88@gmail.com>
2021-01-26 16:57:23 -0300

    Alteracoes no componente EditMovie

--- src/App.test.js
+++ src/App.test.js
@@ -162,7 +162,7 @@ describe('3 - Insira um link para a página de detalhes de um filme dentro de `M
   })
 });
 
-describe.only('4 - Faça uma requisição para buscar o filme que deverá ser renderizado dentro de `Movie Details`', () => {
+describe('4 - Faça uma requisição para buscar o filme que deverá ser renderizado dentro de `Movie Details`', () => {
 
   it('deverá exibir o texto "Carregando..." enquanto estiver fazendo a requisição', async () => {
     for (const movie of readMovies()) {
@@ -209,7 +209,7 @@ describe.only('4 - Faça uma requisição para buscar o filme que deverá ser re
   });
 });
 
-describe('5 - Realize uma requisição para buscar o filme que será editado em `EditMovie`', () => {
+describe.only('5 - Realize uma requisição para buscar o filme que será editado em `EditMovie`', () => {
 
   it('deverá exibir o texto "Carregando..." enquanto estiver fazendo a requisição', async () => {
     for (const movie of readMovies()) {
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -2,29 +2,53 @@ import React, { Component } from 'react';
 
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import { Redirect } from 'react-router-dom';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: [],
+      status: 'loading',
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const result = await movieAPI.getMovie(id);
+    this.setState({
+      movie: result,
+      status:'Ok',
+    });
+  };
+
+  async handleSubmit(updatedMovie) {    
+    console.log(updatedMovie)
+    const result = await movieAPI.updateMovie(updatedMovie);
+    this.setState({      
+      shouldRedirect: true,
+    });
   }
 
   render() {
+
     const { status, shouldRedirect, movie } = this.state;
+    
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect exact to="/" />   
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />
     }
 
     return (
-      <div data-testid="edit-movie">
+      <div data-testid="edit-movie">        
         <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
