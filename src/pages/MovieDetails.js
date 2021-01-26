f08d18046df2f9203e0b021989f64fbe477d8046
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-25 21:27:48 -0300

    Requisito 4 - Adicionando a requisiçao getMovie, renderizando Loading e renderizando o card com as informaçoes dos filmes

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link, Redirect } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // const { loading } = this.state;
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  constructor() {
+    super();
 
-    // if (loading) {
-    //   return <Loading />;
-    // }
+    this.state = {
+      movie: {},
+      status: 'loading',
+      shouldRedirect: false,
+    };
+    this.delete = this.delete.bind(this);
+  }
+
+  componentDidMount() {
+    movieAPI.getMovie(this.props.match.params.id)
+      .then((movie) => this.setState({ movie, status: 'details' }));
+  }
+
+  delete() {
+    const { id } = this.state.movie;
+    movieAPI.deleteMovie(id)
+      .then(() => this.setState({ shouldRedirect: true }));
+  }
+
+  render() {
+    const { movie, status, shouldRedirect } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
+
+    if (shouldRedirect) { return <Redirect to="/" />; }
+    if (status === 'loading') { return <Loading />; }
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Title: ${title}` }</p>
+        <h1>{ `Title: ${title}` }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <a href="/" onClick={ this.delete }>DELETAR</a>
       </div>
     );
   }
 }
 
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
+// consultei https://github.com/tryber/sd-07-project-movie-card-library-crud/blob/08bc22ad390041c9465d970500a5cf2e57b498be/src/pages/MovieDetails.js
 export default MovieDetails;
