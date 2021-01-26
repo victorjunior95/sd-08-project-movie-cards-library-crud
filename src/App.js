5721cdacea4d1707ced53c35f64adca664cd1946
Ronald Jr <rpintojunior@gmail.com>
2021-01-26 15:03:24 -0300

    Insere item 1 - render BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import {
+  MovieList,
+  MovieDetails,
+  NewMovie,
+  NotFound,
+  EditMovie,
+} from './pages';
 
 function App() {
   return (
-    <Router>
-    <div>Movie Card Library CRUD</div>
-  </Router>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
