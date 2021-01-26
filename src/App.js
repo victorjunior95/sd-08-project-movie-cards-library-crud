3e9f95336e090f9fcd74369c4ab5f2b8c20f3f38
Thiago Ferreira <thiagocristhian@gmail.com>
2021-01-26 12:15:34 -0300

    Corrigindo requisito 3

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
     <BrowserRouter>
       <div>
         <Switch>
-          <Route path exact="/" component={ MovieList } />
-          <Route path="/movies/:id" component={ MovieDetails } />
           <Route path="/movies/new" component={ NewMovie } />
           <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path exact="/" component={ MovieList } />
           <Route component={ NotFound } />
         </Switch>
       </div>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { id } } = this.props;
+    const { movie: { id, title, storyline } } = this.props;
     return (
       <div data-testid="movie-card">
+        <h4>{title}</h4>
+        <h4>{storyline}</h4>
         <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
@@ -16,6 +18,8 @@ class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     id: PropTypes.number,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
   }).isRequired,
 };
 
