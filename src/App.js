d90d7421a47597e6eb6017be54021dccb4c3ab0a
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-01-26 10:10:42 -0300

    First commit - Let's go

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        Movie Card Library CRUD
+      </div>
+    );
+  }
 }
 
 export default App;
