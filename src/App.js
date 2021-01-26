f9fd5024c87957e484a360c335ed714d5184098c
Hugo Andrade Braga <hugobrr@gmail.com>
2021-01-26 15:17:08 -0300

    erro airbnb

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {BrowserRouter, Router, Switch ,Redirect} from 'react-router-dom'
+import MovieList from './src/pages/MovieList'
+import MovieDetails from './src/pages/MovieDetails'
+import EditMovie from './src/pages/EditMovie'
+import NewMovie from './src/pages/NewMovie'
+import NotFound from './src/pages/NotFound'
 
+// https://stackoverflow.com/questions/32128978/react-router-no-not-found-route
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
-    // teste
+    <BrowserRouter>
+      <Switch>
+        <Router exact path="/" component={MovieList} />
+        <Router path="/movies/">
+          <Router path="/movies/:id" component={MovieDetails} >
+            <Router path="/movies/:id/edit" component={EditMovie} />
+          </Router>
+          <Router path="/movies/new" component={NewMovie} />
+        </Router>
+      </Switch>
+      <Redirect component={NotFound}/>
+    </BrowserRouter>
   );
 }
 
