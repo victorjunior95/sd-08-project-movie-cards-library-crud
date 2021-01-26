686499b34dffc2b6bf237fa2953a55eb22066965
Silvio Dayube <scarige@gmail.com>
2021-01-26 16:24:31 -0300

    Req 2 - Adds Api Request and Loading Message

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
+import Loading from '../components/Loading';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
     this.state = {
       movies: [],
     };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((allMovies) => this.setState({
+      movies: [...allMovies],
+    }));
   }
 
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (!movies.length) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
