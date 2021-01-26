a4dac13cb200a1627754e8758b85ce3beefea6e8
Maycon Felipe <mayconfelipe.dev@gmail.com>
2021-01-26 18:21:59 -0300

    Requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -7,19 +7,35 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.callCard = this.callCard.bind(this);
+
     this.state = {
       movies: [],
+      loading: false,
     };
   }
 
+  async componentDidMount() {
+    this.callCard();
+  }
+
+  callCard() {
+    this.setState({ loading: true }, () => {
+      movieAPI.getMovies().then((response) => this.setState({
+        movies: response,
+        loading: false,
+      }));
+    });
+  }
+
   render() {
-    const { movies } = this.state;
-
+    const { movies, loading } = this.state;
+    const loadingElement = <span>Carregando...</span>
     // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? loadingElement
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
