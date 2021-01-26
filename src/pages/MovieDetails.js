e1d2ab8a4b2ac544e5072f369a9a518aa0d65bfe
albertassi88 <albertassi88@gmail.com>
2021-01-26 15:46:17 -0300

    Alteracoes no componente MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,21 +2,52 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();    
+
+    this.state = {
+      movies: [],
+    };
+    
+    this.hundleApi = this.hundleApi.bind(this);
+  }
+
+  componentDidMount() {
+    this.hundleApi();
+  }
+
+  async hundleApi() { 
+    const { match: { params: { id } } } = this.props;  
+    const result = await movieAPI.getMovie(id);
+    
+    this.setState({
+      movies: result,
+    });
+  }
+
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    
+    const { movies } = this.state;     ;    
+    const { title, storyline, imagePath, genre, rating, subtitle } = movies;
+ 
     return (
       <div data-testid="movie-details">
+        <Loading />
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>       
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={`/movies/${movies.id}/edit`}>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
