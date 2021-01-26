4b2e8fdc69608b19761b7229dfb71ac47ac8046b
wellington <profwellington17@gmail.com>
2021-01-26 17:25:25 -0300

    fix requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Movielist from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
@@ -10,13 +10,11 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <switch>
-        <Route Path="/" component={Movielist} />
-        <Route path="/movies/:id" render={(props) => <MovieDetails {...props}  /> } />
-        <Route path="/movie/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props}  /> } />
-        <Route Path="/" component={NotFound} />
-      </switch>
+      <Route exact Path="/" component={ Movielist } />
+      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      <Route component={ NotFound } />
     </BrowserRouter>
   );
 }
