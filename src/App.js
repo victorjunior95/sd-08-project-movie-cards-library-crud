ecaddfa593d869767617de3734aaa7a4c42857d2
Silvio Dayube <scarige@gmail.com>
2021-01-26 17:15:41 -0300

    Req 3 - Creates movie cards with link

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" render={ () => <MovieList /> } />
-      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-      <Route path="/movies/new" render={ () => <NewMovie /> } />
-      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
-      <Route path="*" exact render={ () => <NotFound /> } />
+      <Switch>
+        <Route path="/" exact render={ () => <MovieList /> } />
+        <Route path="/movies/new" render={ () => <NewMovie /> } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route path="*" render={ () => <NotFound /> } />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h3>{movie.title}</h3>
+        <p>{movie.storyline}</p>
+        <Link to={ `/movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieCard;
