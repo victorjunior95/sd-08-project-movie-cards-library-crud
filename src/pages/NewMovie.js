793f456aa24e90c1fa63c02ccdb6ead77628150f
albertassi88 <albertassi88@gmail.com>
2021-01-26 17:33:47 -0300

    Alteracoes no componente NewMovie

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -2,17 +2,30 @@ import React, { Component } from 'react';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
+import { Redirect } from 'react-router-dom';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      status: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(newMovie) {
+  async handleSubmit(newMovie) {
+    await movieAPI.createMovie({ ...newMovie });    
+    this.setState({
+      status: true,
+    });
   }
 
   render() {
+    const { status } = this.state;
+    if (status) return <Redirect exact to="/" /> 
+    
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
