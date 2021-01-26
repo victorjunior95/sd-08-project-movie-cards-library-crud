49c964135f609e95ba09358d205c9e238373d08b
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-26 19:25:56 -0300

    :Rocket: first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import MovieList from './pages/MovieList';
+import MovieDatails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <h1>Movie Card Library CRUD</h1>
+      <BrowserRouter>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" render={ (props) => <MovieDatails { ...props } /> } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      </BrowserRouter>
+    </div>
   );
 }
 
