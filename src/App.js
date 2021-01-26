bc175357be0ecbb818554af21667f5ec792146e2
wellington <profwellington17@gmail.com>
2021-01-26 16:08:19 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Movielist from './components/'
+
+import Movielist from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <switch>
-        <Route Path="/Movielist" component={Movielist} />
+        <Route Path="/" component={Movielist} />
         <Route path="/movies/:id" render={(props) => <MovieDetails {...props}  /> } />
+        <Route path="/movie/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props}  /> } />
+        <Route Path="/" component={NotFound} />
       </switch>
     </BrowserRouter>
   );
