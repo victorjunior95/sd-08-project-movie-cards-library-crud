58a9ae227c51d77e86c0b4e264eff2a7dbc1a14b
Bruno Martins <bruno.martins217@gmail.com>
2021-01-26 11:49:26 -0300

    First Try Requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -31,6 +32,7 @@ class MovieList extends Component {
     if (loading) return <Loading />;
     return (
       <div data-testid="movie-list">
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 // import movies from '../services/movieData';
@@ -7,15 +7,21 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      redirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
-    movieAPI.createMovie();
-    return newMovie;
+    movieAPI.createMovie(newMovie).then(() => this.setState({
+      redirect: true,
+    }));
   }
 
   render() {
+    const { redirect } = this.state;
+    if (redirect) return <Redirect to="/" />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
