d5220945fa86dac1d8d5d45fe5e9f216dba8fb8a
Aline Debastiani <debastianialine@gmail.com>
2021-01-25 22:23:21 -0300

    cumpre requisito 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route component={ NotFound } />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
