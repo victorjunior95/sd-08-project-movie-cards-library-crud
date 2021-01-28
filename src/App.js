bd7eb4c28848a96618a110b5b01f384f91ffe2e4
Leonardo <lecastro16@gmail.com>
2021-01-27 21:31:35 -0300

    requisito 1 v0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+// import  { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD Leonardo Sardinha</div>
-    </Router>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
