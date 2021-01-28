29ed100a4ce791156fe95280cd444678077ac1eb
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-01-28 11:37:42 -0300

    req 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import {
+  MovieList,
+  MovieDetails,
+  NewMovie,
+  EditMovie,
+  NotFound
+} from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+     <BrowserRouter>
+     <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />        
+        <Route exact path="/" component={ MovieList } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
