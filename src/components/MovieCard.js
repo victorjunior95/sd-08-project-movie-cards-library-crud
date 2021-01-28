0f323dccbb037550f1eddba5116dfe5aa2590a11
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-01-28 12:51:00 -0300

    create of <BrowserRouter> and yours respectives <Route> in the component <App>

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id } } = this.props;
     return (
       <div data-testid="movie-card">
         Movie Card
@@ -9,5 +12,12 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
