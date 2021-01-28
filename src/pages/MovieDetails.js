f74b447ec779b5f24f3ea55d3d76d687aa55d4d0
Digo-Castro <dygo.castro@gmail.com>
2021-01-27 21:25:52 -0300

    Altera o PropTypes

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -47,4 +47,12 @@ class MovieDetails extends Component {
   }
 }
 
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
 export default MovieDetails;
