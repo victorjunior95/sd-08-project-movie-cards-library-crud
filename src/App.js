289e00b9c920c4189c19111d8ee244065e5f3012
albertassi88 <albertassi88@gmail.com>
2021-01-28 16:04:28 -0300

    Corrigindo erros

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends React.Component {
         <div>Movie Card Library CRUD</div>
         <Switch>
           <Route exact path="/" component={ MovieList } />
-          <Route exact path="/movies/new"
+          <Route
+            path="/movies/new"
             render={ (props) => <NewMovie { ...props } /> }
           />
           <Route
