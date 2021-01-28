167b37389fa1689abafdda9fa3247ed9c8333985
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-01-28 12:49:48 -0300

    Criaçao do BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { EditMovie, NewMovie, MovieDetails, MovieList, NotFound } from './pages'
+import "./App.css";
 function App() {
   return (
-    <h1>Movie Card Library CRUD</h1>
+    <BrowserRouter>
+        <h1>Movie Card Library CRUD</h1>
+        <Switch>
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="/" component={ MovieList } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+    </BrowserRouter>
+
   );
 }
 
