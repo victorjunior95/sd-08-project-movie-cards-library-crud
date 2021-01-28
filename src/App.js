337a546c51bac51984f3166d5407841214100978
Leonnardo Lair <leonnardo0316@gmail.com>
2021-01-27 22:15:37 -0300

    Adding Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Switch>
+        <Router path="/" component={MovieList} />
+        <Router path='/movies/:id' component={MovieDetails} />
+        <Router path="/movies/new" component={NewMovie} />
+        <Router path="/movies/:id/edit" component={EditMovie} />
+        <Router component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
