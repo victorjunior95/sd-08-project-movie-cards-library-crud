efdc37e995d665f1ed302fedf0c901320d909d57
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-01-29 13:10:53 -0300

    add req 1 a req 4

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, storyline, id } } = this.props;
+    const {
+      movie: { title, storyline, id },
+    } = this.props;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <p>{title}</p>
+        <p>{storyline}</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
@@ -17,6 +22,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
     storyline: PropTypes.string,
+    id: PropTypes.number,
   }).isRequired,
 };
 
