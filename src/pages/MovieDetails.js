817608a55b1c791a31d156595a91e961c392ba7c
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-30 12:33:04 -0300

    requisito bonus: adiciona botao para deletar o card e redirecionar para a pagina inicial

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
@@ -22,12 +22,16 @@ class MovieDetails extends Component {
       }));
   }
 
+  async deleteMovie(id) {
+    await movieAPI.deleteMovie(id);
+    this.setState({ redirect: true });
+  }
+
   render() {
-    const { loading, movie } = this.state;
+    const { loading, movie, redirect } = this.state;
     const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
-    if (loading) {
-      return <Loading />;
-    }
+    if (loading) return <Loading />;
+    if (redirect) return <Redirect to="/" />;
     return (
       <div data-testid="movie-details">
         <h1>{ title }</h1>
@@ -38,6 +42,7 @@ class MovieDetails extends Component {
         <p>{ `Rating: ${rating}` }</p>
         <Link to="/">VOLTAR</Link>
         <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <button type="button" onClick={ this.deleteMovie(id) }>DELETAR</button>
       </div>
     );
   }
