b4e5586d1f289f7410987fe71a05257bdb2cfa63
Daniel Leon <daniel.radialista@gmail.com>
2021-01-29 22:34:56 -0300

    requisitos 1 a 5

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages'
 
 function App() {
   return (
-    <>
+    <Router>
       <div>Movie Card Library CRUD</div>
-      <Router>
-        <Switch>
-          <Route path="/movies/new" component={ NewMovie } />
-          <Route path="/movies/:id/edit" component={ EditMovie } />
-          <Route path="/movies/:id" component={ MovieDetails } />
-          <Route exact path="/" component={ MovieList } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </Router>
-    </>
+      <Switch>
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </Router>
   );
 }
 
