b4af75b76ff0c33a997227448aee57ca5d53d6cd
Daniel Leon <daniel.radialista@gmail.com>
2021-01-30 10:31:12 -0300

    requisitos 1 ao 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -14,7 +15,7 @@ class MovieList extends Component {
   }
 
   componentDidMount() {
-    movieAPI.getMovies.then((data) => {
+    movieAPI.getMovies().then((data) => {
       this.setState({
         movies: data,
         loading: false,
@@ -31,6 +32,7 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
