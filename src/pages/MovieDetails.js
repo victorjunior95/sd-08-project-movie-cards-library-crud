287e8b47abf1d09f815b85f37ed43f98573e31f3
Daniel Leon <daniel.radialista@gmail.com>
2021-01-30 18:59:45 -0300

    requisito 7

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
@@ -10,7 +10,10 @@ class MovieDetails extends Component {
     this.state = {
       movie: {},
       loading: true,
+      shouldRedirect: false,
     };
+
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   componentDidMount() {
@@ -23,12 +26,17 @@ class MovieDetails extends Component {
     });
   }
 
+  handleDelete() {
+    const { movie: { id } } = this.state;
+    movieAPI.deleteMovie(id).then(() => this.setState({ shouldRedirect: true }));
+  }
+
   render() {
     // Change the condition to check the state
     const { movie: { title, storyline, imagePath, genre, rating, subtitle, id },
-      loading } = this.state;
+      loading, shouldRedirect } = this.state;
     if (loading) return <Loading />;
-
+    if (shouldRedirect) return <Redirect to="/" />;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
@@ -39,6 +47,7 @@ class MovieDetails extends Component {
         <p>{`Rating: ${rating}`}</p>
         <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
         <Link to="/">VOLTAR</Link>
+        <Link onClick={ this.handleDelete } to="/">DELETAR</Link>
       </div>
     );
   }
