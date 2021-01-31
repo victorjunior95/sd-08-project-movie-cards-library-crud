78565597f3143d54f986381178d5120596e5e0da
Giovanna Betti <giovannabetti@gmail.com>
2021-01-31 04:58:44 -0300

    reslving requisite 03

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline } = movies;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <p>{ title }</p>
+        <p>{ storyline }</p>
       </div>
     );
   }
