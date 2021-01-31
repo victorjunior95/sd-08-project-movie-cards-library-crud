86725610a85237be05c9f79c83269b04d5a22226
Lucas Neiva <lucassneiva.code@gmail.com>
2021-01-31 18:07:34 -0300

    starting the project, req1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDatails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+      <Route path="/" exact component { MovieList } />
+      <Route path="/movies/:id" exact component { MovieDatails } />
+      <Route path="/movies/new" exact component { NewMovie } />
+      <Route path="/movies/:id/edit" exact component { EditMovie } />
+      <Route path="" exact component { NotFound } />
+    </Switch>
     </Router>
   );
 }
