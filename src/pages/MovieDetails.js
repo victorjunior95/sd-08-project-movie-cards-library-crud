92d85543f3d120020b47471154e176e90dfd29c8
Leonardo <lecastro16@gmail.com>
2021-02-01 12:36:58 -0300

    requisito 1 ao 4 v4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -45,6 +45,7 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
