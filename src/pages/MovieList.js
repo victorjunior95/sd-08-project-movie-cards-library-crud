22dcc8d17d286c3e321fe35924e9a809b2c80000
Blucilha <blucilha.nascimento@gmail.com>
2021-02-01 09:34:11 -0300

    Exercicio 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieList extends Component {
   constructor() {
@@ -9,18 +9,41 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.renderMovies = this.renderMovies.bind(this);
+    this.fazRequisicao = this.fazRequisicao.bind(this);
+  }
+
+  componentDidMount() {
+    this.fazRequisicao();
+  }
+
+  async fazRequisicao() {
+    const aguardaRequisicao = await movieAPI.getMovies();
+    this.setState({
+      movies: aguardaRequisicao,
+      loading: false,
+    });
+  }
+
+  renderMovies() {
+    const { movies } = this.state;
+    return (
+      <div
+        data-testid="movie-list"
+      >
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { loading } = this.state;
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      <main>
+        { loading ? <Loading /> : this.renderMovies() }
+      </main>
     );
   }
 }
