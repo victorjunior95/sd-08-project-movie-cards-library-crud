003deb38598218eb37a207575965d86b6d1ed944
Uilton Rocha <itoclick@gmail.com>
2021-02-01 18:55:16 -0300

    requisitos de movie edit

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -45,7 +45,7 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to="/">VOLTAR</Link>
-        <Link to={ `/movies/:${id}/edit` }>EDITAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
         <Link to="/" onClick={ () => movieAPI.deleteMovie(id) }>DELETAR</Link>
       </div>
     );
