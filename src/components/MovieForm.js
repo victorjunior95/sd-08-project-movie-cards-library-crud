db901724f82d38876aaec47e7a14a86f420eceae
Geziel <geziel.fernandez@pucp.pe>
2021-02-01 09:32:51 -0300

    Geziel, why this thing is not working 2

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,16 @@ class MovieForm extends React.Component {
   }
 }
 
+MovieForm.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+  }).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
 export default MovieForm;
