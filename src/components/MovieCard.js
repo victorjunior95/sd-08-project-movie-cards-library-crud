ae47e149605995c3d4877e81a61ce293919df2d0
Blucilha <blucilha.nascimento@gmail.com>
2021-02-01 09:57:30 -0300

    Exercicio 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{ title }</h1>
+        <h4>{ storyline}</h4>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
