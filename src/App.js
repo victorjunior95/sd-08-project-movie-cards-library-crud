6c7b352eed382c7b48d770a8857c4837d317280f
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-02-01 11:12:35 -0300

    Req1: Renderizando BrowserRouter no app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { EditMovie, NewMovie, MovieDetails, MovieList, NotFound } from './pages'
-import "./App.css";
+import { EditMovie, NewMovie, MovieDetails, MovieList, NotFound } from './pages';
+import './App.css';
+
 function App() {
   return (
     <BrowserRouter>
-        <h1>Movie Card Library CRUD</h1>
-        <Switch>
-          <Route path="/movies/:id/edit" component={ EditMovie } />
-          <Route path="/movies/new" component={ NewMovie } />
-          <Route path="/movies/:id" component={ MovieDetails } />
-          <Route path="/" component={ MovieList } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
+      <h1>Movie Card Library CRUD</h1>
+      <Switch>
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     </BrowserRouter>
 
   );
