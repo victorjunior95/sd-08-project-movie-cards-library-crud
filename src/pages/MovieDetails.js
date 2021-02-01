57b083dc8b49ac7337136e183a6ac4eccdfd1bc2
hugobrr <hugobrr@gmail.com>
2021-02-01 09:47:00 -0300

    subindo o 4 requisito após 1 rodada do eslint com 33%

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
@@ -12,28 +13,22 @@ class MovieDetails extends Component {
       isLoading: true,
     };
   }
-  
+
   componentDidMount() {
     const { match: { params: { id } } } = this.props;
-    // let { id } = useParams();
-    movieAPI.getMovie(id).then((answer)=>{
+    movieAPI.getMovie(id).then((answer) => {
       this.setState({
         movie: answer,
         isLoading: false,
-      })
+      });
     });
   }
-  
+
   render() {
     const { match: { params: { id } } } = this.props;
-    // let { id } = useParams();
     const { movie,isLoading } = this.state;
-    //   // Change the condition to check the state
-    // console.log(movie);
     if (isLoading) return <Loading />;
-    // console.log(movie);
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
-    console.log(movie);
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
@@ -43,10 +38,19 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to="/">VOLTAR</Link>
-        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <p>  </p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
+}
+
+MovieDetails.PropTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  })
 }
 
 export default MovieDetails;
