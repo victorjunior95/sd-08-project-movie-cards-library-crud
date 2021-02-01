7b20e947189261d8a32aa12b090dfd0498b5ae6d
Blucilha <blucilha.nascimento@gmail.com>
2021-02-01 08:54:15 -0300

    Exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import { MovieList, MovieDetails, EditMovie, NotFound, NewMovie } from './pages';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Route path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route path="" component={ NotFound } />
     </Router>
   );
 }
