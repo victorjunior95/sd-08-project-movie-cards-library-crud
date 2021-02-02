6709ff7282590e95717e2a6b587ccc5863e59a88
Antônio Schappo <antonioschappo@gmail.com>
2021-02-02 13:21:55 -0300

    Req. 5 done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
-      <Route path="/movies/new" component={ NewMovie } />
-      <Route exact path="/" component={ MovieList } />
-      <Route exact path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-      <Route component={ NotFound } />
+      <Switch>
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
