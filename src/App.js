4a385753da71dee0164146b0f84df31f87243540
Felip França <felipefrancafj@gmail.com>
2021-02-02 09:43:48 -0300

    REQ-1 | Create and Implement Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { EditMovie, NewMovie, MovieDetails, MovieList, NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <div className="CRUD">Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+        <Route component={ NotFound } />
+      </Switch>
+    </Router>
   );
 }
 
