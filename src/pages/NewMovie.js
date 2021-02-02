b45bf1adc0505eb7152cbeabf93bf45fffb4f4a5
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2021-02-02 10:12:44 -0400

    enviando trabalho

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react';
 
 import MovieForm from '../components/MovieForm';
-import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(newMovie) {
-  }
-
   render() {
     return (
       <div data-testid="new-movie">
