5e00dc123bc0cdf90d160f02b320cd6c878e67cc
Daniel <marcio.daniel@msn.com>
2021-02-02 01:29:24 -0300

    Renderize  no componente  usando rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <>
+      <h1> Eu estou aqui no App!</h1>
+      <Router>
+        <Switch>
+          <Route path="/movies/:id/edit/" component={ EditMovie } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route path="*" component={ NotFound } />
+
+        </Switch>
+      </Router>
+    </>
   );
 }
 
