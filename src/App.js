6418edbb57bac4e591e6f1be1537d9e4aa122024
Rafael <rafaeljavaro750@gmail.com>
2021-02-03 17:31:31 -0300

    Pagina MovieList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import MovieList from './pages/MovieList';
+// import MovieDetails from './pages/MovieDetails';
+// import NewMovie from './pages/NewMovie';
+// import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      Movie Card Library CRUD
+      <MovieList />
+      <BrowserRouter>
+        <Switch>
+          {/* <Router exact path="/" component={ MovieList } /> */}
+          {/* <Router path="/movies/:id" component={ MovieDetails } /> */}
+          {/* <Router path="/movies/new" component={ NewMovie } /> */}
+          {/* <Router path="/movies/:id/edit" component={ EditMovie } /> */}
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
+import Loading from '../components/Loading';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
 
-class MovieList extends Component {
+export default class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
       movies: [],
+      loading: false,
     };
+
+    this.showMovies = this.showMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.showMovies();
+  }
+
+  showMovies() {
+    this.setState(
+      () => ({ loading: true }),
+      async () => {
+        const moviesArray = await movieAPI.getMovies();
+        this.setState({
+          loading: false,
+          movies: moviesArray,
+        });
+      },
+    );
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      <div>
+        {
+          loading ? <Loading />
+            : (
+              <div data-testid="movie-list">
+                {console.log(movies)}
+                {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+              </div>
+            )
+        }
+
       </div>
     );
   }
 }
-
-export default MovieList;
