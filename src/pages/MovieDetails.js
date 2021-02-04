5a08607b10c28c8565fa9bc0d570bf7b9a2989b3
Victor Moraes <moraesvictorgk@gmail.com>
2021-02-04 15:18:47 -0300

    first commit

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      load: true,
+      movie: [],
+    };
+    this.spreadMovies = this.spreadMovies.bind(this);
+    this.importMovies = this.importMovies.bind(this);
+  }
+
+  spreadMovies() {
+    const { match: { params: { id } } } = this.props;
+    this.importMovies(movieAPI.getMovies(id));
+  }
+
+  importMovies(final) {
+    this.setState(
+      { load: true },
+      async () => {
+        this.setState({
+          movie: await final,
+          load: false,
+        });
+      },
+    );
+  }
+
+  renderMovieDetails() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
-      <div data-testid="movie-details">
+      <div>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h4>{ `Title: ${title}` }</h4>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <p><Link to="/movies/:id/edit">EDITAR</Link></p>
+        <p><Link to="/">VOLTAR</Link></p>
+      </div>
+    );
+  }
+
+  render() {
+    const { load } = this.state;
+
+    return (
+      <div data-testid="movie-details">
+        {load ? <Loading /> : this.renderMovieDetails()}
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import MovieCard from '../components/MovieCard';
+
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +13,41 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      load: true,
     };
+
+    this.spreadMovies = this.spreadMovies.bind(this);
+    this.importMovies = this.importMovies.bind(this);
+  }
+
+  spreadMovies() {
+    this.importMovies(movieAPI.getMovies());
+  }
+
+  importMovies(final) {
+    this.setState(
+      { load: true },
+      async () => {
+        this.setState({
+          movies: await final,
+          load: false,
+        });
+      },
+    );
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, load } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      <div>
+        <p><Link to="movies/:id">ADICIONAR CARTÃO</Link></p>
+        <div className="movie-list" data-testid="movie-list">
+          {load ? <Loading />
+            : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        </div>
       </div>
     );
   }
