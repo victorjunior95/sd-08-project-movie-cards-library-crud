b70d6d7327dc207ec01d4329095fd8d0faf0009c
Daniel <marcio.daniel@msn.com>
2021-02-03 22:11:10 -0300

    Subindo primeiro e segundo requisito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MovieList, NewMovie, EditMovie, MovieDetails, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Switch>
+        <Route path="/movies/:id/edit/" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,19 +10,43 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: false,
     };
+
+    this.renderMoviesList = this.renderMoviesList.bind(this);
+    this.getApi = this.getApi.bind(this);
   }
 
-  render() {
+  componentDidMount() {
+    this.getApi();
+  }
+
+  async getApi() {
+    const result = await movieAPI.getMovies();
+    this.setState({ movies: result, loading: true });
+
+    return result;
+  }
+
+  renderMoviesList() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
+  }
+
+  render() {
+    const { loading } = this.state;
+    if (loading) {
+      return (
+        <>
+          {this.renderMoviesList()}
+        </>);
+    }
+    return (
+      <Loading />);
   }
 }
 
