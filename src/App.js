6602f5d54969fe2ffb6ae266698e6751ce8c298c
antonio arieiro <sfgfffs@gmail.com>
2021-02-05 12:20:50 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+
     </BrowserRouter>
   );
 }
