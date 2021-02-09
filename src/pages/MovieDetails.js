6859fbc26b009b4c64b3da12599580fd05b0d614
antonioarieiro <sfgfffs@gmail.com>
2021-02-09 11:53:59 -0300

    fix movie detais

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,19 +1,94 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  constructor() {
+    super();
+    this.state = {
+      movie: undefined,
+      done: false,
+    };
+    this.SrcMovie() = this.SrcMovie().bind(this);
+    this.remover = this.remover.bind(this);
+    this.renderDetails = this.renderDetails.bind(this);
+  }
+
+  componentDidMount() {
+    this.SrcMovie();
+  }
+
+  redirectNotFound() {
+    const {
+      history: { push },
+    } = this.props;
+    push('/notfound');
+  }
+
+  async SrcMovie() {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    const data = await movieAPI.getMovie(id);
+
+    this.setState({ movie: data });
+
+    if (!data) {
+      return this.redirectNotFound();
+    }
+    this.setState({ done: true });
+  }
+
+  remover() {
+    const {
+      movie: { id },
+    } = this.state;
+    movieAPI.deleteMovie(id);
+  }
+
+  LinksRender() {
+    const {
+      movie: { id },
+    } = this.state;
     return (
-      <div data-testid="movie-details">
+      <>
+        <Link to="/" onClick={ this.remover }>
+          {' '}
+          DELETAR
+        </Link>
+        <br />
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <br />
+        <Link to="/">VOLTAR</Link>
+      </>
+    );
+  }
+
+  renderDetails() {
+    const {
+      movie: { title, storyline, imagePath, genre, rating, subtitle },
+    } = this.state;
+    return (
+      <>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{`Title: ${title}`}</p>
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        {this.LinksRender()}
+      </>
+    );
+  }
+
+  render() {
+    const { done } = this.state;
+    return (
+      <div data-testid="movie-details">{!done ? <Loading /> : this.renderDetails()}</div>
     );
   }
 }
-
-export default MovieDetails;
