488b380292218b00c18e398644061241a6c23713
Demistocles <demi.treze@gmail.com>
2021-02-09 17:08:17 -0300

    resolvendo o primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route Exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
