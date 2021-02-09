6f618d3fa4a57324712fdda2d94a6eba4f768833
antonioarieiro <sfgfffs@gmail.com>
2021-02-09 11:44:17 -0300

    fix edit movie

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,34 +1,63 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Loading, MovieForm } from '../components';
 
-import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: undefined,
+      done: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
+    this.renderForm = this.renderForm.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    const {
+      history: { push },
+    } = this.props;
+    return push('/');
+  }
+
+  async fetchMovie() {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    const data = await movieAPI.getMovie(id);
+
+    this.setState({ movie: data });
+
+    if (!data) {
+      return this.redirectNotFound();
+    }
+    this.setState({ done: true });
+  }
+
+  redirectNotFound() {
+    const {
+      history: { push },
+    } = this.props;
+    push('/notfound');
+  }
+
+  renderForm() {
+    const { movie } = this.state;
+    return <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />;
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
-
-    if (status === 'loading') {
-      // render Loading
-    }
-
-    return (
-      <div data-testid="edit-movie">
-        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
-      </div>
-    );
+    const { done } = this.state;
+    return <div data-testid="edit-movie">{!done ? <Loading /> : this.renderForm()}</div>;
   }
 }
-
-export default EditMovie;
