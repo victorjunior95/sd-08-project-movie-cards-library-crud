9ef73e6eda98b2f9d6b6e2bcc90348d82e73fa21
Demistocles <demi.treze@gmail.com>
2021-02-09 18:27:07 -0300

    correçao no requisito

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
     <Router>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route Exact path="/" component={ MovieList } />
-        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
         <Route path="*" component={ NotFound } />
       </Switch>
     </Router>
