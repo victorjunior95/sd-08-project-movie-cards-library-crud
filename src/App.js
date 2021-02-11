7f1f981549479253fb983444ab12208acf00a227
antonioarieiro <sfgfffs@gmail.com>
2021-02-11 16:26:19 -0300

    dale

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
-import MovieList from './pages/MovieList';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MovieDetails from './pages/MovieDetails';
-import NewMovie from './pages/NewMovie';
+import MovieList from './pages/MovieList';
 import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
 import NotFound from './pages/NotFound';
-
-import './App.css';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/" exact component={ MovieList } />
-        <Route path="/" component={ NotFound } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </Router>
   );
