f2ef8f20401d218c07b28109f219a1848ffacd1a
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-02-11 14:41:02 -0300

    req1: created routes

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={ MovieList } />
-        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
         <Route component={ NotFound } />
       </Switch>
     </BrowserRouter>
