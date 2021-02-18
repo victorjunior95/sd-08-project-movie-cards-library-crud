b373296ef55a785342eeb222ca4c83d64d499227
Max <gmaxgomes@gmail.com>
2021-02-18 09:50:55 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+
+import MovieList from './pages/MovieList'
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
 import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route path="/" component={ } />
-        <Route path="/movies/new" component={ } />
-        <Route path="/movies/:id" component={ } />
-        <Route path="/movies/:id/:edit" component={ } /> */}
+        <Route path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id/:edit" component={ EditMovie } />
+        <Route path="/*" component={ NotFound }></Route>
       </Switch>
     </BrowserRouter>
   );
