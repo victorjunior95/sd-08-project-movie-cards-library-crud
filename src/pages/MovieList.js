b468c5cbf6ca1fc9a0ff2d4847277c5d59fc7102
Cainan <cainan6697@gmail.com>
2021-02-21 20:45:47 -0300

    link adicionar cartao do requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,6 +3,7 @@ import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
+import { Link } from 'react-router-dom';
 
 class MovieList extends Component {
   constructor() {
@@ -38,6 +39,9 @@ class MovieList extends Component {
       <div className="movie-list" data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.id } movie={ movie } />)}
         <div>{ loading ? <Loading /> : null }</div>
+        <Link className="link" to="/movies/new">
+          ADICIONAR CARTÃO
+        </Link>
       </div>
     );
   }
