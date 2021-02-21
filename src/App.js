30771b6aaee47af30e7d2b86e09764ab7abc806f
Cainan <cainan6697@gmail.com>
2021-02-21 18:28:44 -0300

    issues

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
         <h1 className="page-title">Movie Card Library CRUD</h1>
       </header>
       <Switch>
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route exact path="/" component={ MovieList } />
         <Route path="*" component={ NotFound } />
       </Switch>
