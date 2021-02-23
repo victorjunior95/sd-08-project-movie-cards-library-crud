6b76147f2ed398c968f49b61336b552243637f01
Andre Sampaio <andresampaiolm@gmail.com>
2021-02-23 00:18:46 -0300

    basic structure of routes ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  EditMovie,
+  MovieDetails,
+  MovieList,
+  NewMovie,
+  NotFound,
+} from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/movies/:id' render={(props) => <MovieDetails {...props} />}/>;
+        <Route path='/movies/new' render={(props) => <NewMovie {...props} />}/>;
+        <Route path='/movies/:id/edit' render={(props) => <EditMovie {...props} />}/>;
+        <Route exact path='/' render={(props) => <MovieList {...props} />}/>;
+        <Route render={() => <NotFound />}/>;
+      </Switch>
+    </BrowserRouter>
   );
 }
 
