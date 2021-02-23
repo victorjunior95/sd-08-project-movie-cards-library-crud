86207e78b5d9ca1298a7493275d44bfc68d2bfc6
Andre Sampaio <andresampaiolm@gmail.com>
2021-02-23 00:45:47 -0300

    MovieList page now shows loading message and gets the movies from api

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -7,19 +7,36 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.requestMovies = this.requestMovies.bind(this);
+
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.requestMovies();
+  }
 
-    // Render Loading here if the request is still happening
+  async requestMovies() {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      loading: false,
+      movies,
+    });
+  }
+
+  render() {
+    const { movies, loading } = this.state;
+
+    const loadingElement = <span>Carregando...</span>;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading
+          ? loadingElement
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
