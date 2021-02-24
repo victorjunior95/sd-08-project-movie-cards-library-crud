337f7018e07b3a10d5efc8c075d435e2aae2df7b
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-02-24 10:38:23 -0300

    Task 2 - Locally passing

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,14 +10,24 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then((data) => {
+      this.setState({
+        movies: data,
+        loading: false,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    if (loading) return <Loading />;
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
@@ -24,5 +35,14 @@ class MovieList extends Component {
     );
   }
 }
+
+// função de ciclo de vida, componentDidMount e dentro a gnt faz a requisição
+// para API, acessamos a getMovie, q retorna uma promisse, e atraves do
+// .then e pegamos o retorno dela (data).
+// alterei o texto no componente loading para carregando
+// import do loading. cria o state, p verificar o loading
+// if(loading), retorna o component loading.
+// qdo terminar o loading, ocorre o .then, e set do loading p false
+// que vai trazer e renderizar todos movies, movie card.
 
 export default MovieList;
