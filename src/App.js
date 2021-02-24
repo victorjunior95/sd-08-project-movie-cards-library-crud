89e9e708ec5502d4a71a51f965444ae7632347bb
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-02-24 08:52:07 -0300

    Starting The Project!There is Always time to Ontrackear!Testing GitHub Flow!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+    </Router>
   );
 }
+
+// testing git flow
 
 export default App;
