b7e1bff20f849ad317c397a9b75ae8e6aa4bc6c4
v-icto-r <victor130496@gmail.com>
2021-02-24 18:50:08 -0300

    requisitos 4 e 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
@@ -7,11 +7,13 @@ function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <Route path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
-      <Route path="/movies/new" component={ NewMovie } />
-      <Route path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="" component={ NotFound } />
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="" component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
