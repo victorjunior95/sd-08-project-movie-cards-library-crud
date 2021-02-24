d239f6e6b4a695f1eb8ed3377eeef0930bf6c171
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2021-02-24 10:00:36 -0300

    Task 1 - Locally passing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, EditMovie, NewMovie, NotFound } from './pages';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
 
-// testing git flow
+// posso importar os componentes direto do ./pages, pq foi criado index
+// por isso q da certo o destructuring nesse caso
+// exact path da match somente na "/" raiz
+// doc react determina "*" para notFound
 
 export default App;
