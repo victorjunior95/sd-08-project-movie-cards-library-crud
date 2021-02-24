70d3800b2a0dd8d75d0a905fa118482037e6344a
Erick-Marinho <erickgaves@gmail.com>
2021-02-24 12:35:15 -0300

    Corrigindo requisito 01, inclusão Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // Bibliotecas React
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Components
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={ MovieList } />
-      <Route path="/movies/:id" exact component={ MovieDetails } />
-    </Switch>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={ MovieList } />
+        <Route path="/movies/:id" exact component={ MovieDetails } />
+      </Switch>
+    </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
 // Components
 import App from './App';
 // Style
 import './index.css';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, 
-  document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
