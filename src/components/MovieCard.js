a13067b4f3ff01dc843d85ef2d62b157313c8df9
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-02-25 02:32:15 -0300

    terceiro requisito

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, storyline } = movie;
+    console.log(movie);
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <div>{title}</div>
+        <div>{storyline}</div>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
-
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
+};
 export default MovieCard;
