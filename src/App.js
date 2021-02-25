fa85aab456bdba22e3b8f8c8fcfe7f2688212848
LukeCruz <enio.cruz@yahoo.com.br>
2021-02-25 11:42:23 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {BrowserRouter, router, link, Switch, Link}  from 'react-router-dom';
+import '../App.css';
+import {NewMovie, EditMovie, MovieList, NotFound,MovieDetails} from '../src/pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/" component={NewMovie} />
+          <Route exact path="/" component={MovieDetails} />
+          <Route exact path="/" component={EditMovie} />
+          <Route exact path="/" component={NotFound} />
+        </Switch>
+        <Link to="/movie/new">ADICIONAR CARTAO</Link>
+      </BrowserRouter>
+    </div>
   );
 }
 
