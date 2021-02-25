ad6d92f8954c0b4af79fec987f87a06280190aa4
Erick-Marinho <erickgaves@gmail.com>
2021-02-25 10:28:59 -0300

    Ordenando rotas

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export default function App() {
     <Router>
       <Switch>
         <Route path="/" exact component={ MovieList } />
-        <Route path="/movies/:id" exact component={ MovieDetails } />
         <Route path="/movies/new" exact component={ NewMovie } />
+        <Route path="/movies/:id" exact component={ MovieDetails } />
         <Route path="/movies/:id/edit" exact component={ EditMovie } />
         <Route component={ NotFound } />
       </Switch>
