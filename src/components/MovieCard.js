7c3ddfdfe1ad81847fea696894fc6b44ba3b8ad7
Víctor Cançado <victor.cancado@gmail.com>
2021-02-25 02:18:37 -0300

    Req. 3 Completo

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MovieDetails from '../pages/MovieDetails';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, storyline, } = movie;
+    console.log(movie);
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <div>{title}</div>
+        <div>{storyline}</div>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
